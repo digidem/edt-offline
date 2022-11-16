2b445e401d18612152ee3eab6fe7a7aeeedda3f5
Luandro <luandro@digital-democracy.org>
2022-11-16 16:29:55 -0300

    chore: completely substitute kiwix for pywb

--- services/syncthing/cont-init.d/99-custom-files.sh
+++ services/syncthing/cont-init.d/99-custom-files.sh
@@ -5,8 +5,8 @@ echo "=======" "Adds variables" "====="
 
 sed -i -r "s/#SOURCE_ID/$SYNC_SOURCE_ID/g" /config/config.xml
 sed -i -r "s/#SOURCE_NAME/$SYNC_SOURCE_NAME/g" /config/config.xml
-sed -i -r "s/#KIWIX_ID/$SYNC_KIWIX_ID/g" /config/config.xml
-sed -i -r "s/#KIWIX_NAME/$SYNC_KIWIX_NAME/g" /config/config.xml
+sed -i -r "s/#CRAWLS_ID/$SYNC_CRAWLS_ID/g" /config/config.xml
+sed -i -r "s/#CRAWLS_NAME/$SYNC_CRAWLS_NAME/g" /config/config.xml
 sed -i -r "s/#MAPEO_NAME/$SYNC_MAPEO_NAME/g" /config/config.xml
 sed -i -r "s/#MAPEO_ID/$SYNC_MAPEO_ID/g" /config/config.xml
 sed -i -r "s/#FDROID_ID/$SYNC_FDROID_ID/g" /config/config.xml
@@ -14,6 +14,6 @@ sed -i -r "s/#FDROID_NAME/$SYNC_FDROID_NAME/g" /config/config.xml
 sed -i -r "s/#INSTALLERS_ID/$SYNC_INSTALLERS_ID/g" /config/config.xml
 sed -i -r "s/#INSTALLERS_NAME/$SYNC_INSTALLERS_NAME/g" /config/config.xml
 
-echo "=======" "Creating kiwix folder and setting permissions" "====="
-mkdir -p /config/kiwix
+echo "=======" "Creating crawls folder and setting permissions" "====="
+mkdir -p /config/crawls
 chown -R 1000:1000 /config
