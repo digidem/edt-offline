2e18f8a14a72dead2cb53784c6852666b2220519
Luandro <luandro@digital-democracy.org>
2023-01-26 10:10:31 -0300

    feat: add password to Syncthing, ajust permissions, update filebrowser script

--- services/filebrowser/start.sh
+++ services/filebrowser/start.sh
@@ -2,13 +2,13 @@
 
 echo "--------- Starting FileBrowser -----------"
 echo "Initiating config"
-./filebrowser config init
+filebrowser config init
 echo "Creating new user"
-./filebrowser users add ${ADMIN_LOGIN} ${ADMIN_PASSWORD} --perm.admin --lockPassword
+filebrowser users add ${ADMIN_LOGIN} ${ADMIN_PASSWORD} --perm.admin --lockPassword
 echo "Loading config file"
 echo "LOCALE: $LOCALE"
 echo "Using user: $ADMIN_LOGIN and password: $ADMIN_PASSWORD"
 sed -i -r "s/#LOCALE/$LOCALE/g" /config/custom/filebrowser.json
-./filebrowser config import /config/custom/filebrowser.json
-./filebrowser config set --branding.files "/config/custom/branding" # Not working
-./filebrowser
+filebrowser config import /config/custom/filebrowser.json
+filebrowser config set --branding.files "/config/custom/branding"
+filebrowser
