5be17d8f8c976fc11556d75a0748cb967337de63
Luandro <luandro@digital-democracy.org>
2023-01-30 21:44:33 -0300

    feat: working Terrastories with auto-migration on first run

---
+++ services/terrastories/start.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+file=firstboot
+
+if [ ! -e "$file" ] ; then
+    echo "First run!"
+    echo "================================"
+    echo "Running migration"
+    bin/rails db:migrate db:seed
+    yarn
+    touch "$file"
+fi
+
+if [ ! -w "$file" ] ; then
+    echo "Migration already happened!"
+    exit 1
+fi
+scripts/server
