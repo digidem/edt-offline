9782403d0a330a2f6bc8ab8657488a34b668f5d5
Luandro <luandro@digital-democracy.org>
2023-03-22 20:48:08 -0300

    fix: crawler script modified to run only once

--- scripts/crawl.sh
+++ scripts/crawl.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-docker-compose -f docker/cloud/docker-compose.yml run crawler crawl --config /app/crawl-config.yml
+docker-compose \
+    -f docker/cloud/docker-compose.yml \
+    run \
+    --build \
+    -e CRAWL_TIMER=0 \
+    crawler \
+    crawl --config /app/crawl-config.yml
