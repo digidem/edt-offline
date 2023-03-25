b86e622106edc42dacf9dda2de0e4a1a2c5e9ba2
Luandro <luandro@digital-democracy.org>
2023-03-25 18:50:56 -0300

    chore: use nightly portal and code cleanup

--- services/crawler/start.sh
+++ services/crawler/start.sh
@@ -3,7 +3,7 @@
 echo "Starting Earth Defenders Toolkit crawler"
 # TODO: also run pywb to show what's latest crawl
 echo "With timer $CRAWL_TIMER"
-if [ $CRAWL_TIMER = 0 ]; then
+if [ "$CRAWL_TIMER" = 0 ]; then
 echo "Running once"
 /app/crawl.sh
 fi
