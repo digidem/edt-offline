af0028d999d1dbdce713cc3682e49cfdc4a64d52
Luandro <luandro@digital-democracy.org>
2023-08-08 13:57:58 -0300

    fix(filebrowser): revert to working start.sh

--- services/filebrowser/start.sh
+++ services/filebrowser/start.sh
@@ -4,8 +4,6 @@ echo "--------- Starting FileBrowser -----------"
 echo "Initiating config"
 filebrowser config init
 echo "Creating new user"
-mkdir -p /users
-chown -R 1000:1000 /users
 filebrowser users add ${ADMIN_LOGIN} ${ADMIN_PASSWORD} --perm.admin --lockPassword
 echo "Loading config file"
 echo "LOCALE: $LOCALE"
@@ -13,7 +11,4 @@ echo "Using user: $ADMIN_LOGIN and password: $ADMIN_PASSWORD"
 sed -i -r "s/#LOCALE/$LOCALE/g" /config/custom/filebrowser.json
 filebrowser config import /config/custom/filebrowser.json
 filebrowser config set --branding.files "/config/custom/branding"
-# if [ -n "$FILE_NOAUTH" ]; then
-#   filebrowser config set --auth.method noauth
-# fi
 filebrowser
